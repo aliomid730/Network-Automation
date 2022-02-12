6be6bcadfe6233ba9547f333262fe76c2c22b7b7
Sayyed Ali Agha Hashimi <ali.omid730@gmail.com>
2022-02-12 20:42:23 +0430

    Update auto-net.py

--- auto-net.py
+++ auto-net.py
@@ -1,49 +1,13 @@
 #!/usr/bin/env python
 
 from netmiko import ConnectHandler
+from hosts import *
 
-iosv_l2_s1 = {
-    'device_type': 'cisco_ios',
-    'ip': '172.16.0.61',
-    'username': 'itadmin',
-    'password': 'L3tM3!nPaccw0rd@123',
-}
-
-iosv_l2_s2 = {
-    'device_type': 'cisco_ios',
-    'ip': '172.16.0.59',
-    'username': 'itadmin',
-    'password': 'L3tM3!nPaccw0rd@123',
-}
-
-iosv_l2_s3 = {
-    'device_type': 'cisco_ios',
-    'ip': '172.16.0.56',
-    'username': 'itadmin',
-    'password': 'L3tM3!nPaccw0rd@123',
-}
-
-iosv_l2_s4 = {
-    'device_type': 'cisco_ios',
-    'ip': '172.16.0.58',
-    'username': 'itadmin',
-    'password': 'L3tM3!nPaccw0rd@123',
-}
-
-iosv_l2_s5 = {
-    'device_type': 'cisco_ios',
-    'ip': '172.16.0.57',
-    'username': 'itadmin',
-    'password': 'L3tM3!nPaccw0rd@123',
-}
-
-
-with open('vlans.txt') as f:
+with open('template_config') as f:
     lines = f.read().splitlines()
 print(lines)
 
-
-all_devices = [iosv_l2_s1, iosv_l2_s2, iosv_l2_s3, iosv_l2_s4, iosv_l2_s5]
+all_devices = [s1, s2, s3, s4, s5]
 
 for devices in all_devices:
     net_connect = ConnectHandler(**devices)
